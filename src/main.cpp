ba0eaa039a711bb0c9334d524dffd5e5762df7bd
Goaty1208 <93077779+Goaty1208@users.noreply.github.com>
2023-12-01 19:14:46 +0100

    Graphics Fixed. Fuck's sake.

--- src/main.cpp
+++ src/main.cpp
@@ -5,24 +5,22 @@ const unsigned int windowHeight = 600;
 const unsigned int windowWidth = 600;
 
 //Conversion rates------------------------
-const float i_x = 1.0f;
 const float i_y = 0.5f;
-const float j_x = -1.0f;
 const float j_y = 0.5f;
 //Tile heights and widths. Standard-------
 
 //NOTE: These values should NOT change, regardless of any zooming factor
 
-const int tileWidth = 32;       
-const int tileHeight = 16;      
+const int tileWidth = 64;       
+const int tileHeight = 32;      
 
 //----------------------------------------
 
 
 Vector2 isometricToScreen(Vector2 isoPosition) {
     Vector2 screenPosition;
-    screenPosition.x = (isoPosition.x - isoPosition.y) * (tileWidth * i_x);
-    screenPosition.y = (isoPosition.x + isoPosition.y) * (tileHeight * i_y);
+    screenPosition.x = (isoPosition.x - isoPosition.y) * (tileWidth * i_y);
+    screenPosition.y = (isoPosition.x + isoPosition.y) * (tileHeight * j_y);
     return screenPosition;
 }
 
@@ -30,7 +28,7 @@ Vector2 isometricToScreen(Vector2 isoPosition) {
 
 int main(){
 
-    InitWindow(windowWidth, windowHeight, "Et Erat Bellum - 0.0.0.0.0.0.0.1");
+    InitWindow(windowWidth, windowHeight, "Et Erat Bellum - 0.0.0.0.0.0.0.2");
 
     Image grass = LoadImage("graphics/tiles/grass.png");
     Texture2D grassTexture = LoadTextureFromImage(grass);
@@ -41,7 +39,7 @@ int main(){
         BeginDrawing();
 
         for (int i = 0; i < 8; i++) {
-            for (int j = 0; j < 8; j++){
+            for (int j = 0; j < 8; j++) {
                 Vector2 tilePosition = { static_cast<float>(i), static_cast<float>(j) };
                 Vector2 screenPosition = isometricToScreen(tilePosition);
                 DrawTexture(grassTexture, screenPosition.x, screenPosition.y, WHITE);
@@ -54,6 +52,8 @@ int main(){
         EndDrawing();
     }
     
+    UnloadTexture(grassTexture);
+    UnloadImage(grass);
     CloseWindow();
 
     return 0;
