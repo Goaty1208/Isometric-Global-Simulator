19ec4ba0227200a360e31ce97aaaadaca4ae6c39
Goaty1208 <93077779+Goaty1208@users.noreply.github.com>
2023-12-02 12:24:03 +0100

    Screenshot shitfuckery

--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,17 @@
 #include "raymath.h"
 #include <iostream>
 
+//Resolution thingies---------------------
 const unsigned int windowHeight = 600;
 const unsigned int windowWidth = 600;
+int display = GetCurrentMonitor();
+//----------------------------------------
 
 //Conversion rates------------------------
 const float i_y = 0.5f;
 const float j_y = 0.5f;
 //Tile heights and widths. Standard-------
+
 
 //NOTE: These values should NOT change, regardless of any zooming factor
 
@@ -25,7 +29,7 @@ const int mapSizeExtra = 4096;
 //----------------------------------------
 
 
-//Isometric coordinate converter for tiles.
+//Isometric coordinate to screen converter for tiles.
 Vector2 isometricToScreenTiles(Vector2 isoPosition) {
     Vector2 screenPosition;
     screenPosition.x = (isoPosition.x - isoPosition.y) * (tileWidth * i_y);
@@ -41,13 +45,32 @@ Vector2 isometricToScreen(Vector2 isoPosition) {
     return screenPosition;
 }
 
+//Screen coordinate to isometric converter for tiles.
+Vector2 screenToIsometricTiles(Vector2 isoPosition) {
+    Vector2 screenPosition;
+    screenPosition.x = (isoPosition.x - isoPosition.y) * (tileWidth * i_y);
+    screenPosition.y = (isoPosition.x + isoPosition.y) * (tileHeight * j_y);
+    return screenPosition;
+}
+
+void drawMap(Texture2D texture){
+    for (int i = 0; i < mapSize; i++) { //Tile drawing. Pretty damn primitive
+        for (int j = 0; j < mapSize; j++) {
+            Vector2 tilePosition = { static_cast<float>(i), static_cast<float>(j) };
+            Vector2 screenPosition = isometricToScreenTiles(tilePosition);
+            DrawTexture(texture, screenPosition.x, screenPosition.y, WHITE);
+        }
+    }    
+
+}
+
 
 int main(){
     
     Camera2D camera = { 0 };
     camera.zoom = 1.0f;
 
-    InitWindow(windowWidth, windowHeight, "Et Erat Bellum - 0.0.0.0.0.0.0.4");
+    InitWindow(windowWidth, windowHeight, "Et Erat Bellum - 0.0.0.0.0.0.0.5");
 
     Image grass = LoadImage("graphics/tiles/grass.png");
     Texture2D grassTexture = LoadTextureFromImage(grass);
@@ -57,6 +80,28 @@ int main(){
 
     while (!WindowShouldClose()){
 
+        //Window resolution checking. ------------------------------------------------------------------
+        if ((IsKeyPressed(KEY_ENTER) && (IsKeyDown(KEY_LEFT_ALT) || IsKeyDown(KEY_RIGHT_ALT))))
+ 		{
+            // see what display we are on right now
+ 			int display = GetCurrentMonitor();
+ 
+            
+            if (IsWindowFullscreen())
+            {
+                // if we are full screen, then go back to the windowed size
+                SetWindowSize(windowWidth, windowHeight);
+            }
+            else
+            {
+                // if we are not full screen, set the window size to match the monitor we are on
+                SetWindowSize(GetMonitorWidth(display), GetMonitorHeight(display));
+            }
+ 
+            // toggle the state
+ 			ToggleFullscreen();
+ 		}
+        //Definetly original resolution code stops here -------------------------------------------------
 
         //Camera shitfuckery starts ------------------------------------
         // Translate based on mouse right click
@@ -90,24 +135,24 @@ int main(){
         }
         //Camera ends --------------------------------------------------
 
-
+        //Draw segment.
         BeginDrawing();
 
             ClearBackground(BLACK);
             BeginMode2D(camera);
-            for (int i = 0; i < mapSize; i++) { //Tile drawing. Pretty damn primitive
-                for (int j = 0; j < mapSize; j++) {
-                    Vector2 tilePosition = { static_cast<float>(i), static_cast<float>(j) };
-                    Vector2 screenPosition = isometricToScreenTiles(tilePosition);
-                    DrawTexture(grassTexture, screenPosition.x, screenPosition.y, WHITE);
-                }
-            }
+            
+            drawMap(grassTexture);
 
             EndMode2D();
 
              DrawFPS(1,1);
    
         EndDrawing();
+
+        // Check for screenshot request. Overwrites the default path.
+        if (IsKeyPressed(KEY_F12)) {
+            
+        }
     }
     
     UnloadTexture(grassTexture);
