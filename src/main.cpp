432ef893d6a170bb164d83d123ce1115f49ecdfc
Goaty1208 <93077779+Goaty1208@users.noreply.github.com>
2023-12-02 11:20:38 +0100

    Camera, somehow.

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include "raylib.h"
+#include "raymath.h"
 #include <iostream>
 
 const unsigned int windowHeight = 600;
@@ -25,30 +26,72 @@ Vector2 isometricToScreen(Vector2 isoPosition) {
 }
 
 
-
 int main(){
+    
+    Camera2D camera = { 0 };
+    camera.zoom = 1.0f;
 
-    InitWindow(windowWidth, windowHeight, "Et Erat Bellum - 0.0.0.0.0.0.0.2");
+    InitWindow(windowWidth, windowHeight, "Et Erat Bellum - 0.0.0.0.0.0.0.4");
 
     Image grass = LoadImage("graphics/tiles/grass.png");
     Texture2D grassTexture = LoadTextureFromImage(grass);
+
+    int mapSize = 90;
     
     SetTargetFPS(60);
 
     while (!WindowShouldClose()){
-        BeginDrawing();
 
-        for (int i = 0; i < 8; i++) {
-            for (int j = 0; j < 8; j++) {
-                Vector2 tilePosition = { static_cast<float>(i), static_cast<float>(j) };
-                Vector2 screenPosition = isometricToScreen(tilePosition);
-                DrawTexture(grassTexture, screenPosition.x, screenPosition.y, WHITE);
-            }
+
+        //Camera shitfuckery starts ------------------------------------
+        // Translate based on mouse right click
+        if (IsMouseButtonDown(MOUSE_BUTTON_RIGHT))
+        {
+            Vector2 delta = GetMouseDelta();
+            delta = Vector2Scale(delta, -1.0f/camera.zoom);
+
+            camera.target = Vector2Add(camera.target, delta);
         }
 
-        DrawFPS(1,1);
+        // Zoom based on mouse wheel
+        float wheel = GetMouseWheelMove();
+        if (wheel != 0)
+        {
+            // Get the world point that is under the mouse
+            Vector2 mouseWorldPos = GetScreenToWorld2D(GetMousePosition(), camera);
+            
+            // Set the offset to where the mouse is
+            camera.offset = GetMousePosition();
 
-        ClearBackground(BLACK);
+            // Set the target to match, so that the camera maps the world space point 
+            // under the cursor to the screen space point under the cursor at any zoom
+            camera.target = mouseWorldPos;
+
+            // Zoom increment
+            const float zoomIncrement = 0.125f;
+
+            camera.zoom += (wheel*zoomIncrement);
+            if (camera.zoom < zoomIncrement) camera.zoom = zoomIncrement;
+        }
+        //Camera ends --------------------------------------------------
+
+
+        BeginDrawing();
+
+            ClearBackground(BLACK);
+            BeginMode2D(camera);
+            for (int i = 0; i < mapSize; i++) { //Tile drawing. Pretty damn primitive
+                for (int j = 0; j < mapSize; j++) {
+                    Vector2 tilePosition = { static_cast<float>(i), static_cast<float>(j) };
+                    Vector2 screenPosition = isometricToScreen(tilePosition);
+                    DrawTexture(grassTexture, screenPosition.x, screenPosition.y, WHITE);
+                }
+            }
+
+            EndMode2D();
+
+             DrawFPS(1,1);
+   
         EndDrawing();
     }
     
