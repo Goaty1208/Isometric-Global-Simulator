64b87f449c23be23ef8073b2d76e79ff8aface5c
Goaty1208 <93077779+Goaty1208@users.noreply.github.com>
2023-12-06 16:30:27 +0100

    Perlin noise magic: the unjaggification

--- include/mapArithmetics.hpp
+++ include/mapArithmetics.hpp
@@ -28,7 +28,7 @@ const int tileHeight = 32;
 //----------------------------------------
 
 //Map sizes ------------------------------
-const int mapDebug = 64;
+const int mapDebug = 256;
 const int mapSize = 256;
 const int mapSizeMedium = 1024;
 const int mapSizeLarge = 2048;
@@ -54,5 +54,114 @@ int mapDebugTiles[mapDebug][mapDebug]{
     {0,0,0,0,1,1,1,0,0,0},
     {0,0,0,0,0,0,0,0,0,0}
 } ;
+
+
+//Isometric coordinate to screen converter for tiles.
+Vector2 isometricToScreenTiles(Vector2 isoPosition) {
+    Vector2 screenPosition;
+    screenPosition.x = (isoPosition.x - isoPosition.y) * (tileWidth * i_y);
+    screenPosition.y = (isoPosition.x + isoPosition.y) * (tileHeight * j_y);
+    return screenPosition;
+}
+
+//Isometric coordinate converter.
+Vector2 isometricToScreen(Vector2 isoPosition) {
+    Vector2 screenPosition;
+    screenPosition.x = (isoPosition.x - isoPosition.y) * (tileWidth * i_y);
+    screenPosition.y = (isoPosition.x + isoPosition.y) * (tileHeight * j_y);
+    return screenPosition;
+}
+
+//Screen coordinate to isometric converter for tiles.
+//TODO: Implement. Yeah, this shit doesn't work yet.
+Vector2 screenToIsometricTiles(Vector2 isoPosition) {
+    Vector2 screenPosition;
+    screenPosition.x = (isoPosition.x - isoPosition.y) * (tileWidth * i_y);
+    screenPosition.y = (isoPosition.x + isoPosition.y) * (tileHeight * j_y);
+    return screenPosition;
+}
+
+void drawMap(Texture2D grassTexture, Texture2D waterTexture, int drawSize, int** map){
+    for (int i = 0; i < drawSize; i++) { //Tile drawing. Pretty damn primitive
+        for (int j = 0; j < drawSize; j++) {
+            Vector2 tilePosition = { static_cast<float>(i), static_cast<float>(j) };
+            Vector2 screenPosition = isometricToScreenTiles(tilePosition);
+            switch (map[i][j])
+            {
+            case grass:
+                DrawTexture(grassTexture, screenPosition.x, screenPosition.y, WHITE);
+                break;
+            
+            case water:
+                DrawTexture(waterTexture, screenPosition.x, screenPosition.y, WHITE);
+                break;
+            default:
+                DrawTexture(waterTexture, screenPosition.x, screenPosition.y, WHITE);
+                break;
+            }
+                        
+        }
+    }    
+
+}
+
+//Perlin Noise Generator -----------------------------------------------------------------
+Vector2 randomGradient(int ix, int iy) {
+    const unsigned w = 8 * sizeof(unsigned);
+    const unsigned s = w / 2;
+    unsigned a = ix, b = iy;
+    a *= 3284157443;
+
+    b ^= a << s | a >> w - s;
+    b *= 1911520717;
+
+    a ^= b << s | b >> w - s;
+    a *= 2048419325;
+    float random = a * (3.14159265 / ~(~0u >> 1)); // in [0, 2*Pi]
+
+    // Create the vector from the angle
+    Vector2 v;
+    v.x = sin(random);
+    v.y = cos(random);
+
+    return v;
+}
+
+float dotGridGradient(int ix, int iy, float x, float y) {
+    Vector2 gradient = randomGradient(ix, iy);
+
+    float dx = x - static_cast<float>(ix);
+    float dy = y - static_cast<float>(iy);
+
+    return (dx * gradient.x + dy * gradient.y);
+}
+
+float cubicInterpolation(float a0, float a1, float w) {
+    return (a1 - a0) * (3.0 - w * 2.0) * w * w + a0;
+}
+
+float perlinGeneration(float x, float y) {
+    int x0 = static_cast<int>(x);
+    int y0 = static_cast<int>(y);
+    int x1 = x0 + 1;
+    int y1 = y0 + 1;
+
+    // Interpolation weights
+    float sx = x - static_cast<float>(x0);
+    float sy = y - static_cast<float>(y0);
+
+    // Interpolate top
+    float n0 = dotGridGradient(x0, y0, x, y);
+    float n1 = dotGridGradient(x1, y0, x, y);
+    float ix0 = cubicInterpolation(n0, n1, sx);
+
+    // Interpolate bottom
+    n0 = dotGridGradient(x0, y1, x, y);
+    n1 = dotGridGradient(x1, y1, x, y);
+    float ix1 = cubicInterpolation(n0, n1, sx);
+
+    // Interpolate top and bottom
+    return (cubicInterpolation(ix0, ix1, sy) + 1.0f) / 2.0f;
+}
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -5,109 +5,12 @@
 #include "mapArithmetics.hpp"
 
 
-//Isometric coordinate to screen converter for tiles.
-Vector2 isometricToScreenTiles(Vector2 isoPosition) {
-    Vector2 screenPosition;
-    screenPosition.x = (isoPosition.x - isoPosition.y) * (tileWidth * i_y);
-    screenPosition.y = (isoPosition.x + isoPosition.y) * (tileHeight * j_y);
-    return screenPosition;
-}
-
-//Isometric coordinate converter.
-Vector2 isometricToScreen(Vector2 isoPosition) {
-    Vector2 screenPosition;
-    screenPosition.x = (isoPosition.x - isoPosition.y) * (tileWidth * i_y);
-    screenPosition.y = (isoPosition.x + isoPosition.y) * (tileHeight * j_y);
-    return screenPosition;
-}
-
-//Screen coordinate to isometric converter for tiles.
-//TODO: Implement. Yeah, this shit doesn't work yet.
-Vector2 screenToIsometricTiles(Vector2 isoPosition) {
-    Vector2 screenPosition;
-    screenPosition.x = (isoPosition.x - isoPosition.y) * (tileWidth * i_y);
-    screenPosition.y = (isoPosition.x + isoPosition.y) * (tileHeight * j_y);
-    return screenPosition;
-}
-
-void drawMap(Texture2D grassTexture, Texture2D waterTexture, int drawSize, int** map){
-    for (int i = 0; i < drawSize; i++) { //Tile drawing. Pretty damn primitive
-        for (int j = 0; j < drawSize; j++) {
-            Vector2 tilePosition = { static_cast<float>(i), static_cast<float>(j) };
-            Vector2 screenPosition = isometricToScreenTiles(tilePosition);
-            switch (map[i][j])
-            {
-            case grass:
-                DrawTexture(grassTexture, screenPosition.x, screenPosition.y, WHITE);
-                break;
-            
-            case water:
-                DrawTexture(waterTexture, screenPosition.x, screenPosition.y, WHITE);
-                break;
-            default:
-                DrawTexture(waterTexture, screenPosition.x, screenPosition.y, WHITE);
-                break;
-            }
-                        
-        }
-    }    
-
-}
-
-//What in the C++ is this shit -----------------------
-int** generateMap(int mapSize) {
-
-    srand(static_cast<unsigned int>(time(nullptr)));
-
-    float offsetX = static_cast<float>(rand()) / RAND_MAX * 1000.0f;
-    float offsetY = static_cast<float>(rand()) / RAND_MAX * 1000.0f;
-    float scale = static_cast<float>(rand()) / RAND_MAX * 8.0f;
-
-    Image perlinNoise = GenImagePerlinNoise(mapDebug, mapDebug, offsetX, offsetY, scale);
-
-    int** map = new int*[mapSize];
-
-    for (int i = 0; i < mapSize; ++i) {
-
-        map[i] = new int[mapSize];
-
-        for (int j = 0; j < mapSize; ++j) {
-
-            //Funky magic shite
-            Color pixelColor = GetImageColor(perlinNoise, i, j);
-
-            // Normalize the color values to a range suitable for your tile IDs
-            float normalizedValue = (pixelColor.r + pixelColor.g + pixelColor.b) / (3.0f * 255.0f);
-            std::cout << "Normalized Value at (" << i << ", " << j << "): " << normalizedValue << std::endl;
-
-            // Assign grass or water based on a threshold
-            map[i][j] = (normalizedValue < 0.5f) ? grass : water;
-        }
-    }
-
-        UnloadImage(perlinNoise);
-
-    return map;
-}
-
-//I like memory leaks. Note: this function must use the same map size as the size of the map it wants to delete, as otherwise things might very well explode.
-void delete2DIntArray(int** map, int mapSize) {
-    for (int i = 0; i < mapSize; ++i) {
-
-        delete[] map[i];
-    }
-
-    delete[] map;
-}
-
-//----------------------------------------------------
-
 int main(){
     
     Camera2D camera = { 0 };
     camera.zoom = 1.0f;
 
-    InitWindow(windowWidth, windowHeight, "Isometric Global Simulator- 0.0.0.0.0.0.0.8");
+    InitWindow(windowWidth, windowHeight, "Isometric Global Simulator- 0.0.0.0.0.0.0.9");
 
     Image grass = LoadImage("graphics/tiles/grass.png");
     Texture2D grassTexture = LoadTextureFromImage(grass);
