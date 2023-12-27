982ad7cc06b420fd10625bb3293d80dca06d3789
Goaty1208 <93077779+Goaty1208@users.noreply.github.com>
2023-12-27 12:48:54 +0100

    Fucking memory leaks

--- src/engine/mapGeneration.cpp
+++ src/engine/mapGeneration.cpp
@@ -1,5 +1,6 @@
 #include "engine/mapGeneration.hpp"
 
+
 int* generateMap(int mapSize) {
 
     int* map = new int[mapSize];
@@ -10,13 +11,20 @@ int* generateMap(int mapSize) {
     float offsetY = static_cast<float>(rand()) / RAND_MAX * 1000.0f;
     float scale = static_cast<float>(rand()) / RAND_MAX * 80.0f;
 
-    std::cout << "[Info IGS] Generating\n";
+    if (map == nullptr) {
+        std::cout << "[Error IGS] Memory allocation failed\n";
+    }
+
+    std::cout << "[Info IGS] Generating\n"; //Game crashes here. Why? no fucking clue
 
     for (int i = 0; i < mapSize; ++i) {
 
         for (int j = 0; j < mapSize; ++j) {
+
             float perlinValue = perlinGeneration(offsetX + i / scale, offsetY + j / scale);
+            std::cout << "i: " << i << ", j: " << j << ", perlinValue: " << perlinValue << "\n";
             map[index2Dto1D(i, j, mapSize)] = (perlinValue < 0.5f) ? grass : water;
+            
         }
     }
 
